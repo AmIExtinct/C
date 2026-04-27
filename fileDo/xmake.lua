5d4a15f9899c20963c5eb0fa88371b50875457c1
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-04-27 18:07:04 +0000

    Isolating

--- fileDo/xmake.lua
+++
@@ -1,6 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-target("slay")
-    set_kind("binary")
-    add_files("src/*.c")
-
--- what/heads/ll.h
+++
@@ -1,8 +0,0 @@
-#ifndef ll_h
-#define ll_h
-
-struct BaseNode;
-
-struct BaseNode *CreateNode(char *str);
-
-#endif 
