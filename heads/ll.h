7d2cd48664e77fd7ed0852affcd4e1bb38df9290
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-04-27 18:09:06 +0000

    Node Tree Bruh

---
+++ heads/ll.h
@@ -0,0 +1,8 @@
+#ifndef ll_h
+#define ll_h
+
+struct BaseNode;
+
+struct BaseNode *CreateNode(char *str);
+
+#endif 
--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,34 @@
 add_rules("mode.debug", "mode.release")
 
-target("slay")
+target("what")
     set_kind("binary")
-    add_files("src/*.c")
+    add_files("*.c")
+    add_files("src/**.c")
+
+--
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
 
